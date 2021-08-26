99dc626067bd3f663d17f33f1ac6e333f6b03218
Hiori Kino <kino.hiori@nims.go.jp>
2021-08-26 15:18:37 +0900

    read internal cx sample

--- prog/cx_network_main.py
+++ prog/cx_network_main.py
@@ -13,14 +13,21 @@ if __name__ == "__main__":
     import argparse
     if True:
         parser = argparse.ArgumentParser()
-        parser.add_argument("--show_workflow",
-                            help="show workflows", action="store_true")
-        parser.add_argument(
-            "--show_is_a", help="show is-a relation", action="store_true")
-        parser.add_argument("--output_filename", help="output name",
+        parser.add_argument("--output_filename", help="output name (default: output.cx)",
                             default="output.cx")
-        parser.add_argument("--name", help="Network name",
+        parser.add_argument("--name", help="Network name (default: fd_network)",
                             default="fd_network")
+        parser.add_argument("--graphviz_workflow",
+                            help="show workflows", action="store_true")
+        parser.add_argument("--graphviz_is_a",
+                            help="show is-a relation", action="store_true")
+        parser.add_argument("--graphviz_FD_network", action="store_true",
+                            help="show graphviz FD network",
+                            default=False)
+        parser.add_argument("--cx_FD_network",
+                            help="write cytoscape FD network in the CX format",
+                            action="store_true")
+
         parser.add_argument("file", nargs="+")
         args = parser.parse_args()
 
@@ -34,14 +41,16 @@ if __name__ == "__main__":
             type_ = "workflow"
         cxreader = cxReader(filename)
         cxreader.make_network(type_)
-        if type_ == "workflow" and args.show_workflow:
+        if type_ == "workflow" and args.graphviz_workflow:
             cxreader.graphviz_workflow(filename+".png")
-        if type_ == "is-a" and args.show_is_a:
+        if type_ == "is-a" and args.graphviz_is_a:
             cxreader.graphviz_workflow(filename+".png", rankdir="TB")
 
         cxreader_list.append(cxreader)
 
     plotter = FDNPlotter(cxreader_list)
 
-    # plotter.graphviz_fd_network("df_network")
-    plotter.cx_fd_network(args.name, args.output_filename)
+    if args.graphviz_FD_network:
+        plotter.graphviz_fd_network("df_network")
+    if args.cx_FD_network:
+        plotter.cx_fd_network(args.name, args.output_filename)
